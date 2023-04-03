c592ef3d3dff9201b04d6059f73f530e6c9a8cc9
Vangelis <ev.fotiades@yahoo.com>
2023-04-03 16:38:24 +0300

    fetch pokemons from API

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-const fetch = require("node-fetch");
-
-const getShowById = async (id) => {
-  const response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`
-  );
+// const fetch = require("node-fetch");
+const api_url = `https://pokeapi.co/api/v2/pokemon`;
+// const api_ais_url = `https://data.aishub.net/ws.php?username=A&format=B&output=C&compress=D&latmin=E&latmax=F&lonmin=G&lonmax=H&mmsi=I&imo=J&interval=K`;
+// getShowById();
+// const navitia_api = `https://api.navitia.io/v1/coverage/{regions.id}/stop_areas/{stop_areas.id}/arrivals`;
+const pokemonContainer = document.getElementById("pokemon-box");
+// const pokemonItem = document.getElementById("item");
+const getPokemon = async () => {
+  const response = await fetch(api_url);
   const data = await response.json();
-  return data;
+  const { results } = data;
+  console.log(results);
+  console.log(results[3].name);
+  // console.log(abilities);
+  for (let i = 0; i < results.length; i++) {
+    const name = results[i].name;
+    console.log(results[i].url);
+    const pokemonElement = document.createElement("div");
+    pokemonElement.innerHTML = `<p>Pokemon-${
+      i + 1
+    }: <span id="name-${i}">${name}</span></p>`;
+    pokemonContainer.appendChild(pokemonElement);
+  }
 };
-// export default getShowById;
 
-getShowById(2)
-  .then((response) => console.log(response))
-  .catch((error) => console.log(error));
+getPokemon();
 
