fcd1d880a92ae0a4ca92c6957f45590353226b38
Vangelis <ev.fotiades@yahoo.com>
2023-04-04 11:54:29 +0300

    fetch abilities and moves

--- index.js
+++ index.js
@@ -11,14 +11,45 @@ const getPokemon = async () => {
   const { results } = data;
   console.log(results);
   console.log(results[3].name);
-  // console.log(abilities);
   for (let i = 0; i < results.length; i++) {
-    const name = results[i].name;
+    // const name = results[i].name;
     console.log(results[i].url);
+    const url = results[i].url;
+    const pokemonResponse = await fetch(url);
+    const pokemonData = await pokemonResponse.json();
+    const name = pokemonData.name;
+    const frontDefaultSprite = pokemonData.sprites.front_default;
+    const abilities = pokemonData.abilities;
+    console.log(abilities);
+    const moves = pokemonData.moves;
+    console.log(moves);
+    // fetch the png file
+    const dreamWorldSprite =
+      pokemonData.sprites.other.dream_world.front_default;
+    // fetch the abilities
+    const abilityNames = [];
+    for (const ability of abilities) {
+      abilityNames.push(ability.ability.name);
+    }
+    console.log(abilityNames);
+    // fetch the moves
+    const movesNames = [];
+    for (const move of moves) {
+      movesNames.push(move.move.name);
+    }
+    console.log(movesNames);
+    // create a div
     const pokemonElement = document.createElement("div");
-    pokemonElement.innerHTML = `<p>Pokemon-${
+    pokemonElement.classList.add("pokeCard");
+    pokemonElement.innerHTML = `
+    <img src="${dreamWorldSprite}">  
+    <p>Pokemon-${
       i + 1
-    }: <span id="name-${i}">${name}</span></p>`;
+    } / <span id="name-${i}"><strong>${name}</strong></span></p>
+    <p class="abilities">Abilities: ${abilityNames.join(", ")}</p>   
+    <p class="moves">Moves: ${movesNames.join(", ")}</p>  
+    <img src="${frontDefaultSprite}">
+    </div>`;
     pokemonContainer.appendChild(pokemonElement);
   }
 };
